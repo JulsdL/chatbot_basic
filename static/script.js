325fdeb234ccc2d6345908203fe03f347650a794
Julien de Lambilly <julien.delambilly@gmail.com>
2023-04-22 16:01:16 +0200

    Added waiting for answer animation

--- static/script.js
+++ static/script.js
@@ -2,13 +2,21 @@ const userInput = document.getElementById("user-input");
 const sendBtn = document.getElementById("send-btn");
 const chatMessages = document.getElementById("chat-messages");
 
-sendBtn.addEventListener("click", async () => {
+function sendMessage() {
   const question = userInput.value.trim();
   if (question) {
     addMessage("user", question);
     userInput.value = "";
-    const response = await fetchChatbotResponse(question);
-    addMessage("assistant", response);
+    fetchChatbotResponse(question)
+      .then(response => addMessage("assistant", response));
+  }
+}
+
+sendBtn.addEventListener("click", sendMessage);
+
+userInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    sendMessage();
   }
 });
 
@@ -20,7 +28,30 @@ function addMessage(role, content) {
   chatMessages.scrollTop = chatMessages.scrollHeight;
 }
 
+function showTypingIndicator() {
+  const message = document.createElement("div");
+  message.className = "message typing-indicator-container";
+  message.innerHTML = `
+    <span class="assistant">Assistant:</span>
+    <div class="typing-indicator">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>`;
+  chatMessages.appendChild(message);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+}
+
+function hideTypingIndicator() {
+  const typingIndicator = document.querySelector(".typing-indicator-container");
+  if (typingIndicator) {
+    chatMessages.removeChild(typingIndicator);
+  }
+}
+
 async function fetchChatbotResponse(question) {
+  showTypingIndicator();
+
   const response = await fetch("/chat", {
     method: "POST",
     headers: {
@@ -28,6 +59,8 @@ async function fetchChatbotResponse(question) {
     },
     body: JSON.stringify({ question })
   });
+
+  hideTypingIndicator();
 
   const data = await response.json();
   return data.response;
